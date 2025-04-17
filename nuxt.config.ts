8f321d315476b67b209c437fc16222b61281e85a
azizkhonteacher <azizkhonteacher@gmail.com>
2025-04-17 06:17:32 +0500

    API ga ulandi

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,16 @@ export default defineNuxtConfig({
   modules: ["@pinia/nuxt"],
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  css: ["~/assets/style/main.scss"]
+  css: ["~/assets/style/main.scss"],
+  vite: {
+    server: {
+      proxy: {
+        '/api': {
+          target: 'https://redoxai-production.up.railway.app',
+          changeOrigin: true,
+          rewrite: path => path.replace(/^\/api/, '/api'),
+        },
+      },
+    },
+  },
 });
